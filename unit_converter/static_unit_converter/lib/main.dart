765a4cd8fe6a491a401996e8f0d7f4a246d8413c
Mary Xia <maryxia@google.com>
2017-09-20 16:48:52 -0700

    load in units from json

--- unit_converter/static_unit_converter/lib/main.dart
+++ unit_converter/static_unit_converter/lib/main.dart
@@ -1,35 +1,75 @@
+import 'dart:core';
 import 'package:flutter/material.dart';
+import 'dart:convert';
 
 void main() {
   runApp(new UnitConverter());
 }
 
 class UnitConverter extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       title: 'Unit Converter',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: new UnitList(title: 'Unit Converter'),
+      home: new UnitList(),
     );
   }
 }
 
 class UnitList extends StatelessWidget {
-  UnitList({Key key, this.title}) : super(key: key);
+  UnitList({Key key}) : super(key: key);
+  TextStyle categoryTextStyle = new TextStyle(
+    color: Colors.white,
+    fontSize: 20.0,
+  );
 
-  final String title;
+  Widget buildCategory(String category) {
+    return new Container(
+      height: 120.0,
+      margin: const EdgeInsets.all(4.0),
+        color: Colors.lightGreen,
+        child: new Text(
+          category,
+          style: categoryTextStyle,
+        ),
+    );
+  }
 
+  Widget buildUnit(String unit) {
+    return new Container(
+      height: 80.0,
+      margin: const EdgeInsets.symmetric(horizontal: 16.0, vertical: 4.0),
+      color: Colors.green,
+      child: new Text(
+        unit,
+        style: categoryTextStyle,
+      ),
+    );
+  }
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-      appBar: new AppBar(
-        title: new Text(title),
+      body: new Center(
+        child: new FutureBuilder(
+            future:
+                DefaultAssetBundle.of(context).loadString('assets/units.json'),
+            builder: (context, snapshot) {
+              if (snapshot != null && snapshot.data != null) {
+                final JsonDecoder decoder = const JsonDecoder();
+                Map<String, List<Map<String, dynamic>>> data = decoder.convert(snapshot.data);
+                List<Widget> categories = [];
+                for (String key in data.keys) {
+                  categories.add(buildCategory(key));
+                  for (int i = 0; i < data[key].length; i++) {
+                    print(data[key][i]['name']);
+                    categories.add(buildUnit(data[key][i]['name']));
+                  }
+                }
+                return new ListView(children: categories);
+              }
+              return new Text('Loading');
+            }),
       ),
-      body: new Container(),
     );
   }
 }
