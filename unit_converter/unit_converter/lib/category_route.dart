577a70eea26c7db12b9f9ff6785ad98ea97b0cb6
Mary Xia <maryxia@google.com>
2017-10-02 13:30:41 -0700

    change Widget to var

--- unit_converter/unit_converter/lib/category_route.dart
+++ unit_converter/unit_converter/lib/category_route.dart
@@ -30,19 +30,17 @@ class _CategoryRouteState extends State<CategoryRoute> {
         future: DefaultAssetBundle.of(context).loadString('assets/units.json'),
         builder: (context, snapshot) {
           if (snapshot != null && snapshot.data != null) {
-            final JsonDecoder decoder = const JsonDecoder();
+            final decoder = const JsonDecoder();
             Map<String, List<Map<String, dynamic>>> data =
                 decoder.convert(snapshot.data);
-            List<Widget> categoryList = [];
             for (String key in data.keys) {
               List<Unit> units = [];
               for (int i = 0; i < data[key].length; i++) {
                 units.add(new Unit(data[key][i]['name'],
                     data[key][i]['conversion'], data[key][i]['description']));
               }
-              Category category = new Category(key, units);
+              var category = new Category(key, units);
               categories.add(category);
-              categoryList.add(category);
             }
             // TODO responsive
             return new GridView.count(
