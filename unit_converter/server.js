e7a877552b2bcce1ae5f2d10e492e9c7410abd33
Mary Xia <maryxia@google.com>
2017-10-15 15:37:27 -0700

    update descriptions

--- unit_converter/server.js
+++ unit_converter/server.js
@@ -7,41 +7,29 @@ const server = http.createServer((req, res) => {
     if (req.url === '/currency') {
         const data = {
             'units': [
-                {'name': 'Dollar',
+                {'name': 'US Dollar',
                  'conversion': 1.0,
-                 'description': 'The dollar menu was created long before the US dollar.'
+                 'description': 'It is a little-known fact that the production cost of each item in the Dollar Menu is the true driver for the fluctuations in the US Dollar.'
                 },
-                {'name': 'Canadian Dollars',
-                 'conversion': (Math.random() * 0.3 + 0.8).toFixed(2),
-                 'description': 'The dollar menu was created long before the US dollar.'
+                {'name': 'Brownie Points',
+                 'conversion': Math.random() * 0.3 + 0.8,
+                 'description': 'While not a "monetary" currency, Brownie Points are a form of social currency that include doing favors and helping others.'
                 },
                 {'name': 'Bitcoin',
-                 'conversion': (Math.random() * 0.00018779342).toFixed(2),
-                 'description': 'The dollar menu was created long before the US dollar.'
-                },
-                {'name': 'Indian Rupee',
-                 'conversion': (Math.random() * 4.0 + 65.0).toFixed(2),
-                 'description': 'The dollar menu was created long before the US dollar.'
-                },
-                {'name': 'Euro',
-                 'conversion': (Math.random() * 0.2 + 0.8).toFixed(2),
-                 'description': 'The dollar menu was created long before the US dollar.'
-                },
-                {'name': 'Mongolian Tugrik',
-                 'conversion': (Math.random() * 30.0 + 2461.0).toFixed(2),
-                 'description': 'The dollar menu was created long before the US dollar.'
+                 'conversion': Math.random() * 0.00018779342,
+                 'description': 'A decentralized digital currency that can be created by "mining".'
                 },
                 {'name': 'Galleon',
-                 'conversion': (Math.random() * 0.005 + 0.02).toFixed(2),
-                 'description': 'The dollar menu was created long before the US dollar.'
+                 'conversion': Math.random() * 0.005 + 0.02,
+                 'description': "In the Magical Wizarding World, everyone carries around giant bags of Galleons, which doesn't seem practical but sure is idyllic."
+                },
+                {'name': 'Gold Bar',
+                 'conversion': Math.random() * 0.00003 + 0.00078988941,
+                 'description': 'When exchanging for a bar of gold, it is always important to specify the bar-size. Here, we use the 400-troy-ounce, but there also exists the kilobar, and various gram-sized bars.'
                 },
                 {'name': 'Zimbabwean Dollar',
-                 'conversion': (Math.random() * 27000.0 + 2000000000.0).toFixed(2),
-                 'description': 'The Zimbabwean Dollar was subjected to hyperinflation.'
-                },
-                {'name': 'Bar of Gold',
-                 'conversion': (Math.random() * 0.00003 + 0.00078988941).toFixed(2),
-                 'description': ''
+                 'conversion': Math.random() * 27000.0 + 2000000000.0,
+                 'description': 'The Zimbabwean Dollar is a textbook example of hyperinflation.'
                 }
             ]
         };
