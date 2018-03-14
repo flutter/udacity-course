6b7a641f08ea1329fe8690fe5fd27e9d7ff2c85a
Mary Xia <maryxia@google.com>
2018-03-14 11:59:40 -0700

    2 spaces

--- course/01_hello_rectangle/solution_01_hello_rectangle/lib/main.dart
+++ course/01_hello_rectangle/solution_01_hello_rectangle/lib/main.dart
@@ -5,34 +5,34 @@
 import 'package:flutter/material.dart';
 
 void main() {
- runApp(
-   MaterialApp(
-     home: Scaffold(
-       appBar: AppBar(
-         title: Text('Hello Rectangle'),
-       ),
-       body: HelloRectangle(),
-     ),
-   ),
- );
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello Rectangle'),
+        ),
+        body: HelloRectangle(),
+      ),
+    ),
+  );
 }
 
 class HelloRectangle extends StatelessWidget {
- @override
- Widget build(BuildContext context) {
-   return Center(
-     child: Container(
-       color: Colors.greenAccent,
-       height: 400.0,
-       width: 300.0,
-       child: Center(
-         child: Text(
-           'Hello!',
-           style: TextStyle(fontSize: 40.0),
-           textAlign: TextAlign.center,
-         ),
-       ),
-     ),
-   );
- }
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        color: Colors.greenAccent,
+        height: 400.0,
+        width: 300.0,
+        child: Center(
+          child: Text(
+            'Hello!',
+            style: TextStyle(fontSize: 40.0),
+            textAlign: TextAlign.center,
+          ),
+        ),
+      ),
+    );
+  }
 }
