9a8fccee9f0fef832b67fe3437f38e8ac58a31d3
Mary Xia <maryxia@google.com>
2018-04-01 23:15:06 -0700

    Change 04 solution to Dart 2, update UX

--- course/04_navigation/solution_04_navigation/lib/converter_route.dart
+++ course/04_navigation/solution_04_navigation/lib/converter_route.dart
@@ -8,6 +8,8 @@ import 'package:meta/meta.dart';
 import 'package:solution_04_navigation/unit.dart';
 
 /// Converter route (page) where users can input amounts to convert.
+///
+/// Currently, it just displays a list of mock units.
 class ConverterRoute extends StatelessWidget {
   /// Color for this [Category].
   final Color color;
@@ -18,30 +20,31 @@ class ConverterRoute extends StatelessWidget {
   /// Units for this [Category].
   final List<Unit> units;
 
-  /// Constructor.
+  /// This [ConverterRoute] handles [Unit]s for a specific [Category].
+  // TODO: Pass in the [Category]'s name and color; we'll use them later
   const ConverterRoute({
     Key key,
     @required this.name,
     @required this.color,
     @required this.units,
-  })
-      : super(key: key);
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     // Here is just a placeholder for a list of mock units
     final unitWidgets = units.map((Unit unit) {
-      return new Container(
+      // TODO: Add the color for this Container
+      return Container(
         color: color,
-        margin: const EdgeInsets.all(8.0),
-        padding: const EdgeInsets.all(16.0),
-        child: new Column(
+        margin: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(16.0),
+        child: Column(
           children: <Widget>[
-            new Text(
+            Text(
               unit.name,
               style: Theme.of(context).textTheme.headline,
             ),
-            new Text(
+            Text(
               'Conversion: ${unit.conversion}',
               style: Theme.of(context).textTheme.subhead,
             ),
@@ -49,7 +52,7 @@ class ConverterRoute extends StatelessWidget {
         ),
       );
     }).toList();
-    return new ListView(
+    return ListView(
       children: unitWidgets,
     );
   }
